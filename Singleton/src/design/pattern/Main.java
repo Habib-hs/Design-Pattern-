b9f93afe6465588a804eb2e016553001b2224e09
Habib-hs <mhabibullahhs@gmail.com>
2024-08-02 18:28:12 +0600

    test

--- Singleton/src/design/pattern/Main.java
+++ Singleton/src/design/pattern/Main.java
@@ -3,6 +3,6 @@ package design.pattern;
 public class Main {
 
     public static void main(String[] args) {
-	    System.out.println("singleton Design Pattern learning!");
+	    System.out.println("singn learning!");
     }
 }
